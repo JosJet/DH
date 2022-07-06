e542252e8c408578c763d872fa0e144e42c95465
JosJet <89114731+JosJet@users.noreply.github.com>
2022-07-06 09:17:21 -0400

    Update items.ts

--- data/mods/gen2crystalseviiislands/items.ts
+++ data/mods/gen2crystalseviiislands/items.ts
@@ -152,7 +152,6 @@ export const Items: {[itemid: string]: ItemData} = {
 				pokemon.useItem();
 			}
 		},
-		duration: 10,
 		num: 1009,
 		gen: 2,
 		shortDesc: "At the end of every turn, holder restores 1/8 of its max HP. Lasts 10 turns.",
