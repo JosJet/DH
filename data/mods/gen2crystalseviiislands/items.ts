a73d45fa052b207a96a163e80d91ed64f5084cb7
JosJet <89114731+JosJet@users.noreply.github.com>
2022-07-08 09:00:17 -0400

    Update items.ts

--- data/mods/gen2crystalseviiislands/items.ts
+++ data/mods/gen2crystalseviiislands/items.ts
@@ -10,8 +10,8 @@ export const Items: {[itemid: string]: ItemData} = {
 				return false; // skip charge turn
 			}
 		},
-		onAfterHit(source, target, move) {
-				let damage = this.getDamage(source, target, move);
+		onHit(target, source, move) {
+				let damage = this.getDamage(target, source, move);
 				this.damage(1, source, target, 'recoil');
 		},
 		itemUser: ["Dodrio", "Doduo"],
